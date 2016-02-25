9ff36277ba142dc5655c067774a53107e0720beb
airhop <airhop12@gmail.com>
2016-02-25 11:59:21 -0600

    Update index.js

--- NodeLab/routes/index.js
+++ NodeLab/routes/index.js
@@ -1,30 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.sendFile('weather.html', { root:  'public' });
 });
-router.get('/getcity',function(req,res,next) {
-var fs = require('fs');
+router.get('/getcity',function(req,res) {
+
+var myRe = new RegExp("^" + req.query.q);
+
 fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
             if(err) throw err;
             var cities = data.toString().split("\n");
+	    var jsonresult = [];
             for(var i = 0; i < cities.length; i++) {
-            }
-          })
-var myRe = new RegExp("^" + req.query.q);
-var result = cities[i].search(myRe);
-          if(result != -1) {
-          }
-var jsonresult = [];
-        for(var i = 0; i < cities.length; i++) {
-          var result = cities[i].search(myRe); 
-          if(result != -1) {
-           jsonresult.push({city:cities[i]});
-          } 
-        }   
+               var result = cities[i].search(myRe); 
+               if(result != -1) {
+               jsonresult.push({city:cities[i]});
+            } 
+        }  
 res.status(200).json(jsonresult);
+          });
 
 });
 
